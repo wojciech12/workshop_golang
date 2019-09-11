a746a8d77a117b441d0d575594c9c78d9e096ca9
wojciech12 <wojciech.barczynski@hypatos.ai>
2019-09-11 17:07:49 +0200

    Golang and K8S

---
+++ 4_kubernetes/src/example_1.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	var kubeconfig *string
+	home := os.Getenv("HOME")
+	if home == "" {
+		panic("HOME not found")
+	}
+
+	kubeconfig = flag.String("kubeconfig",
+		filepath.Join(home, ".kube", "config"),
+		"(optional) absolute path to the kubeconfig file")
+
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		check(clientset, "kube-system", "etcd-minikube")
+	}
+}
+
+func check(clientset *kubernetes.Clientset, namespace string, pod string) {
+	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+			pod, namespace, statusError.ErrStatus.Message)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	}
+
+	time.Sleep(10 * time.Second)
+}
